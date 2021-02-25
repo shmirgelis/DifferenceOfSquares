7c7644cede466b94eb925c91975429278bc5f3f9
shmirgelis <73270957+shmirgelis@users.noreply.github.com>
2021-02-25 21:04:41 +0200

    amended 6th and 11th line

--- DifferenceOfSquares.cs
+++ DifferenceOfSquares.cs
@@ -3,17 +3,17 @@ using System.Linq;
 
 public static class DifferenceOfSquares
 {
-    const int StartNum = 1;
+    private const int _startNum = 1;
     public static int CalculateSquareOfSum(int max)
     {
-        var numsInRange = Enumerable.Range(StartNum, max);
+        var numsInRange = Enumerable.Range(_startNum, max);
         var sumOfNumsInRange = numsInRange.Sum(x => x);
-        return sumOfNumsInRange *= sumOfNumsInRange;
+        return sumOfNumsInRange*sumOfNumsInRange;
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-        var numsInRange = Enumerable.Range(StartNum, max);
+        var numsInRange = Enumerable.Range(_startNum, max);
         return numsInRange.Sum(x => x * x);
     }
 
